6ac82ae6414ef71d1095645feec1ce78c40c19cf
Romario82 <34745928+Romario82@users.noreply.github.com>
2025-09-03 13:14:15 +0300

    Update most_active_user.py

--- most_active_user.py
+++ most_active_user.py
@@ -7,6 +7,25 @@ logs = [
     "2025-08-25 12:21:15 Анна /catalog",
     "2025-08-25 12:25:30 Иван /product/777",
 ]
+# - 1)
+def most_active_user(logs):
+    data_l = []
+    data_d = {}
+    for i_log in logs:
+        data_l.append(i_log.split()[2])
+    for user in data_l:
+        data_d[user] = data_d.get(user, 0) + 1
+    return max(data_d.items(), key=lambda x: x[1])
+    
+most_active_user = most_active_user(logs)
+print(most_active_user)
 
+# -2)
+def most_active_user(logs):
+    data_d = {}
+    for i_log in logs:
+        user = i_log.split()[2] 
+        data_d[user] = data_d.get(user, 0) + 1
+    return max(data_d.items(), key=lambda x: x[1])
 
 # -> ("Иван", 4)
